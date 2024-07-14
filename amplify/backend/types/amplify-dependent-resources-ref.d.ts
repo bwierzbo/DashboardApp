699fb7484fb019555467426533066e4a67bf3154
Benjamin Wierzbanowski <39303255+bwierzbo@users.noreply.github.com>
2024-07-14 19:10:25 -0400

    back to pnpm making new repo for aws

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "nextjsdashboardd98eaf1b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
